2a735718832745df3b97f415ee44a88a2536004c
mackalexj <110355367+mackalexj@users.noreply.github.com>
2022-08-01 21:07:47 -0400

    A whole bunch of changes to get questrade verification token (#3)
    * more dynamic port var in application.js
    
    * update readme
    
    * add redirection route
    
    * attempted to remove local errors?!
    
    * mmmmmmmmmmmore heroku changes
    
    * many more changes to make this more heroku-able
    
    * moar changes
    
    * really wasn't thinking about change tracking here, sorry
    
    * new start page
    
    * .gitignore update
    
    * again, gitignore changes move consumerkey to hidden file
    
    * add return code path test
    
    * more staging tests
    
    * add more logging to routes
    
    * changes
    
    * local testing fixes
    
    * url path changes
    
    * Another test
    
    * ch changes
    
    * another attempt
    
    * letsgo
    
    * more changes
    
    * tested getting code url back and extracting param with redirect
    
    * changes for getting questrade code in production

--- README.md
+++ README.md
@@ -1,6 +1,26 @@
 # application
 this is an application
 
-npm install express
+this application allows you to use node js, express, and javascript with heroku
+
+it also allows you to 
+
+# some-commands-you-might-want:
+
+npm init
+
+npm install express --save
+
+npm install axios --save
+
+npm install dotenv --save
 
 node application.js
+
+# launching-notes
+
+if you connect heroku to your github branch, and launch your application, this will work (as of this writing)
+
+# source
+(1) - https://www.freecodecamp.org/news/how-to-deploy-a-nodejs-app-to-heroku-from-github-without-installing-heroku-on-your-machine-433bec770efe/
+(2) - https://github.com/heroku/node-js-getting-started
--- application.js
+++ application.js
@@ -1,18 +1,118 @@
 //
-// application: application
+// application: oauth node js heroku express
 //
 
-const express = require('express');
+//source : https://masteringjs.io/tutorials/fundamentals/enum
+class DevelopmentProfile {
+    static Local = new DevelopmentProfile('Local');
+    static Staging = new DevelopmentProfile('Staging');
+    static Production = new DevelopmentProfile('Production');
 
-const PORT = 3000;
+    constructor(name) {
+        this.name = name;
+    };
+    toString() {
+        return `Color.${this.name}`;
+    };
+};
+
+const PROFILE = process.env.PROFILE || DevelopmentProfile.Local.name;
+const express = require('express');
+const axios = require("axios");
+
+const PORT = process.env.PORT || 3000;
 const app = express();
 
-var applicationStartedListeningOnPort = 'Investing app started and listening at port ' + PORT.toString();
+const clientId = readClientId();
 
 app.listen(PORT, (err) => {
     if (err) {
         console.log(err);
     } else {
-        console.log(applicationStartedListeningOnPort);
+        console.log('Development Profile: ' + PROFILE);
+        console.log('Investing app started and listening at port ' + PORT.toString());
     }
 });
+
+app.get('/', (req, res) => {
+    res.writeHead(200, { 'Content-Type':'text/html'});
+    res.end('<div><a href=\'/start\'><p>Start<p></a></div>');
+});
+
+app.get('/start', (req, res) => {
+    var questradeOauthUrlRedirect = createQuestradeOauthUrlRedirect(clientId);
+    authorizationRedirect(questradeOauthUrlRedirect, res);
+});
+
+
+app.get('/questradeCode', (req, res) => {
+    var questradeCode = req.query.code;
+    console.log('Entering method: app.get questradeCode for code: ' + questradeCode);
+    
+    exchangeCodeForAccessToken(questradeCode);
+    res.send('have reached the end');
+});
+
+function exchangeCodeForAccessToken(questradeCode) {
+    var buildResponsePath = buildResponsePath('/accessGranted');
+    var postUrl = 'https://login.questrade.com/oauth2/token?client_id=' + clientId + '&code=' + questradeCode + '&grant_type=authorization_code&redirect_uri=' + buildResponsePath;
+    
+}
+
+app.get('/end', (req, res) => {
+    res.send('Have reached the end');
+});
+
+app.get('/loltest', (req, res) => {
+    console.log('WE MADE IT');
+    res.render('Nice! We Made It');
+});
+
+
+function readClientId() {
+    // will need to get your client Id from questrade
+    // this is your consumer key, as per questrade documentation:
+    // https://www.questrade.com/api/documentation/getting-started
+    // in this case, once you have your consumer key, 
+    // add it in config vars on heroku front end
+    // in this case saving as CONSUMER_KEY
+    console.log('Entering method: readClientId()');
+    const consumerKey = process.env.CONSUMER_KEY || 'test-consumer-key';
+    console.log('Returned Client ID aka Consumer Key: ' + consumerKey);
+    return consumerKey;
+};
+
+function createQuestradeOauthUrlRedirect(clientId) {
+    // need to add URL to questrade 
+    // login to your account
+    // under the app you've registered, add it to call back url's
+    console.log('Entering method: createQuestradeOauthUrlRedirect(' + clientId + ')');
+    // var responseUrl = 'https://questrade-application-testing.herokuapp.com/'
+    var responsePath = buildResponsePath('/questradecode');
+    console.log('Response Path is:' + responsePath);
+    var questradeOauthUrlRedirect = 'https://login.questrade.com/oauth2/authorize?client_id=' + clientId + '&response_type=code&redirect_uri=' + responsePath;
+    console.log('Returned URL for redirect is: ' + questradeOauthUrlRedirect);
+    return questradeOauthUrlRedirect;
+};
+
+function buildResponsePath(pagePath) {
+    var responsePath;
+    if (PROFILE == DevelopmentProfile.Local.name) {
+        responsePath = 'http://localhost:' + PORT + pagePath;
+    } else {
+        responsePath = 'https://questrade-application-testing.herokuapp.com' + pagePath;
+    }
+    return responsePath;
+};
+
+function authorizationRedirect(questradeOauthUrlRedirect, res) {
+    console.log('Entering method: authorizationRedirect()');
+    if (PROFILE === DevelopmentProfile.Local.name) {
+        console.log('Entering method path: Local Testing authorizationRedirect()');
+        res.redirect('http://localhost:' + PORT + '/questradeCode?code=testing123testing321');
+    } else {
+        console.log('Redirecting to URL: ' + questradeOauthUrlRedirect);
+        res.redirect(questradeOauthUrlRedirect);
+    }
+};
+
