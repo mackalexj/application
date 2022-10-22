c41f905b70298f6a2120526a0844df82a6510ce6
mackalexj <mack.alexj@gmail.com>
2022-10-22 17:49:25 -0400

    Add development profile file

---
+++ developmentProfile.js
@@ -0,0 +1,18 @@
+// class to represent string values of the Development Profile
+//
+//source : https://masteringjs.io/tutorials/fundamentals/enum
+class DevelopmentProfile {
+    static Local = new DevelopmentProfile('Local');
+    static Staging = new DevelopmentProfile('Staging');
+    static Production = new DevelopmentProfile('Production');
+
+    constructor(name) {
+        this.name = name;
+    };
+    toString() {
+        return `Color.${this.name}`;
+    };
+};
+
+export {DevelopmentProfile};
+
